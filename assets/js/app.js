465fdc8e8dd76b5b2d0ea589fbe97bec5773e191
Tusar <tusarahmed141@gmail.com>
2022-06-09 09:07:03 +0600

    Take user input by prompt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 // SetTime out
-setTimeout(() => {
-  console.log('Good Morning, Tusar');
-}, 3500)
+// setTimeout(() => {
+//   console.log('Good Morning, Tusar');
+// }, 3500)
+
+
+// Prompt
+const userInp = window.prompt('Enter your number', '');
+const number = parseFloat(userInp);
+const result = `${number + 200}`;
+alert(result)
