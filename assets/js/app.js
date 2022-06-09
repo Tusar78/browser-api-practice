c5221893fd8cff608ddf2e5e98a06568c27a30c2
Tusar <tusarahmed141@gmail.com>
2022-06-09 09:10:47 +0600

    User check by confirm method

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,15 @@
 
 
 // Prompt
-const userInp = window.prompt('Enter your number', '');
-const number = parseFloat(userInp);
-const result = `${number + 200}`;
-alert(result)
+// const userInp = window.prompt('Enter your number', '');
+// const number = parseFloat(userInp);
+// const result = `${number + 200}`;
+// alert(result)
+
+// Confirm 
+const userCheck = confirm('Are you this website link?');
+if (userCheck) {
+  console.log(location.href);
+} else {
+  alert('Dure Gia Mor! (DGM)');
+}
